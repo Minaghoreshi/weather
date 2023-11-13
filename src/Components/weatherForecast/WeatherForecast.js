44badf2c130b36da5c3e682d806a7f73de1b7414
mina ghoreshi <goreishi45@gmail.com>
2023-11-14 02:41:45 +0330

    feat: create forecast cards base on api data

--- src/Components/weatherForecast/WeatherForecast.js
+++ src/Components/weatherForecast/WeatherForecast.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import styles from "./WeatherForecast.module.css";
+import { WeatherContext } from "../../weather-context/weatherContext";
+import { fetchWeatherData } from "../../api/weather-api";
+
 import { LuSunMedium } from "react-icons/lu";
+const apiKey = `6108e90940f04f92ab8192419231311`;
 export function WeatherForecast() {
+  const { state, dispatch } = useContext(WeatherContext);
+  useEffect(() => {
+    fetchWeatherData(dispatch, state.location, apiKey);
+  }, []);
+  let forecastData;
+  if (state.weatherData) {
+    forecastData = state.weatherData.forecast.forecastday;
+    console.log(forecastData);
+  }
+
   return (
     <div className={styles.forecast__wrapper}>
-      <div className={styles.forecast__cart}>
-        <LuSunMedium className={styles.forecast__cart__icon} />
-        <span>Tue</span>
-        <span>30 °C</span>
-      </div>{" "}
-      <div className={styles.forecast__cart}>
-        <LuSunMedium className={styles.forecast__cart__icon} />
-        <span>Tue</span>
-        <span>30 °C</span>
-      </div>{" "}
-      <div className={styles.forecast__cart}>
-        <LuSunMedium className={styles.forecast__cart__icon} />
-        <span>Tue</span>
-        <span>30 °C</span>
-      </div>
+      {forecastData
+        ? forecastData.map((day, index) => (
+            <div className={styles.forecast__cart} key={index}>
+              <LuSunMedium className={styles.forecast__cart__icon} />
+              <span>Tue</span>
+              <span>30 °C</span>
+            </div>
+          ))
+        : ""}
     </div>
   );
 }
