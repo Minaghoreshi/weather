0be90df295b3ea024ad9f3f866df7d1d46a94ec9
mina ghoreshi <goreishi45@gmail.com>
2023-11-14 17:34:57 +0330

    style: remove search input style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import { AppMain } from "./Components/appMain/AppMain";
+// import { AppMain } from "./Components/appMain/AppMain";
 import { WeatherProvider } from "./weather-context/weatherContext";
-
+import { AppMain } from "./Components";
 function App() {
   return (
     <WeatherProvider>
